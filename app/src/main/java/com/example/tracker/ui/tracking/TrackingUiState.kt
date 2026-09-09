51f47e5532732f9fbf256f8c70bb8a32c509d0a8
diddi4956 <diddi4956@naver.com>
2026-09-09 12:47:33 +0900

    9/9 트래킹뷰모델에 날짜 하루씩 옮기는거 추가

--- app/src/main/java/com/example/tracker/ui/tracking/TrackingUiState.kt
+++ app/src/main/java/com/example/tracker/ui/tracking/TrackingUiState.kt
@@ -24,7 +24,7 @@ data class TrackingUiState (
     // expense
     val expenseTrackingOption: List<String> = emptyList(), // loadTrackingData() {체크형리스트, 카테고리별 원그래프 등등의 리스트}
 
-    val selectCategory: List<String> = List("5"), // loadTrackingData() {tracking, circleGraphingByCategory공용}
+    val selectCategory: List<String> = emptyList(), // loadTrackingData() {tracking, circleGraphingByCategory공용}
     val expenseTracking: List<ExpenseTrackingDto> = emptyList(),
     // val subCategory: List<String>, // circleGraphingByCategory에서 빼서 쓰기..
 
--- app/src/main/java/com/example/tracker/ui/tracking/TrackingViewModel.kt
+++ app/src/main/java/com/example/tracker/ui/tracking/TrackingViewModel.kt
@@ -14,20 +14,26 @@ import java.util.Date
 import java.util.Locale
 import androidx.compose.runtime.setValue
 import com.example.tracker.data.dao.ConditionDefinitionDao
+import java.util.Calendar
 
 class TrackingViewModel (
     private val expenseRecordDao: ExpenseRecordDao, // 주생성자의 매개변수
     private val habitRecordDao: HabitRecordDao,
     private val conditionRecordDao: ConditionRecordDao,
     private val conditionDefinitionDao: ConditionDefinitionDao
-
 ) : ViewModel(){
     var trackingUiState by mutableStateOf(TrackingUiState())
         private set
 
+    private val dateFormat = SimpleDateFormat(
+        "yyyy-MM-dd",
+        Locale.getDefault()
+    )
+
     init { // 하 기간을 해야하는거군 하 놔
         val today = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault() ). format (Date())
-        changeDate(today)
+        changePeriod(today, today)
+        loadTrackingData()
     }
 
 //    fun changeDate(date: String){
@@ -38,72 +44,119 @@ class TrackingViewModel (
     fun changePeriod(startDate: String, endDate: String){
         trackingUiState = trackingUiState.copy(startDate = startDate, endDate = endDate)
     }
+
+    fun extendPeriodToPast(){
+        val startDate = dateFormat.parse(trackingUiState.startDate) ?:return
+
+        val calendar = Calendar.getInstance()
+        calendar.time = startDate
+        calendar.add(Calendar.DAY_OF_MONTH, -1)
+
+        trackingUiState = trackingUiState.copy(startDate = dateFormat.format(calendar.time))
+    }
+
+    fun extendPeriodToFuture(){
+        val endDate = dateFormat.parse(trackingUiState.endDate) ?:return
+
+        val calendar = Calendar.getInstance()
+        calendar.time = endDate
+        calendar.add(Calendar.DAY_OF_MONTH, 1)
+
+        trackingUiState = trackingUiState.copy(endDate = dateFormat.format(calendar.time))
+    }
+
     fun loadTrackingData(){
         viewModelScope.launch{
-            val date = trackingUiState.date
+            val startDate = trackingUiState.startDate
+            val endDate = trackingUiState.endDate
             // val selectCategory = 이건 빈리스트가 아니어야하는데
+
             val conditionTagList = conditionDefinitionDao.getConditionTagList()
 
-            trackingUiState = trackingUiState.copy(date = date, getConditionTagList = conditionTagList)
+            trackingUiState = trackingUiState.copy(getConditionTagList = conditionTagList)
         }
     }
 
     // expense
-    fun expenseTracking(start: String, end: String){
+    fun expenseTracking(){
         viewModelScope.launch{
+            val start = trackingUiState.startDate
+            val end = trackingUiState.endDate
+
             val tracking = expenseRecordDao.tracking(start, end)
             trackingUiState = trackingUiState.copy(expenseTracking = tracking)
         }
     }
 
-    fun circleGraphingByCategory(start: String, end: String, categoryId: Long){
+    fun circleGraphingByCategory(categoryId: Long){
         viewModelScope.launch{
+            val start = trackingUiState.startDate
+            val end = trackingUiState.endDate
+
             val circleGraph = expenseRecordDao.circleGraphingByCategory(start, end, categoryId)
             trackingUiState = trackingUiState.copy(circleGraphingByCategory = circleGraph)
         }
     }
 
-    fun wholeCircleGraphing(start: String, end: String){
+    fun wholeCircleGraphing(){
         viewModelScope.launch{
+            val start = trackingUiState.startDate
+            val end = trackingUiState.endDate
+
             val graph = expenseRecordDao.wholeCircleGraphing(start, end)
             trackingUiState = trackingUiState.copy(wholeCircleGraphing = graph)
         }
     }
 
-    fun calcDailyExpense(start: String, end: String){
+    fun calcDailyExpense(){
         viewModelScope.launch{
+            val start = trackingUiState.startDate
+            val end = trackingUiState.endDate
+
             val graph = expenseRecordDao.calcDailyExpense(start, end)
             trackingUiState = trackingUiState.copy(calcDailyExpense= graph)
         }
     }
 
     // habit
-    fun habitTrackingByDefinition(start: String, end: String){
+    fun habitTrackingByDefinition(){
         viewModelScope.launch{
+            val start = trackingUiState.startDate
+            val end = trackingUiState.endDate
+
             val width = habitRecordDao.trackingByDefinition(start, end)
             val length = habitRecordDao.getDefinitionList(start, end)
             trackingUiState = trackingUiState.copy(habitTrackingByDefinition = width, habitDefinitionList = length)
         }
     }
 
-    fun habitTrackingByCategory(start: String, end: String){
+    fun habitTrackingByCategory(){
         viewModelScope.launch {
+            val start = trackingUiState.startDate
+            val end = trackingUiState.endDate
+
             val width = habitRecordDao.trackingByCategory(start, end)
             val length = habitRecordDao.getCategoryList(start, end)
             trackingUiState = trackingUiState.copy(habitTrackingByCategory = width, habitCategoryList = length)
         }
     }
 
-    fun monthlyByCategory(categoryId: Long, start: String, end:String){
+    fun monthlyByCategory(categoryId: Long){
         viewModelScope.launch {
+            val start = trackingUiState.startDate
+            val end = trackingUiState.endDate
+
             val monthlyData = habitRecordDao.getMonthlyByCategory(categoryId, start, end)
             trackingUiState = trackingUiState.copy(monthlyByCategory = monthlyData)
         }
     }
 
     // condition
-    fun conditionTrackingByDefinition(start:String, end: String, tags: List<ConditionTag>){
+    fun conditionTrackingByDefinition(tags: List<ConditionTag>){
         viewModelScope.launch {
+            val start = trackingUiState.startDate
+            val end = trackingUiState.endDate
+
             val width = conditionRecordDao.trackingByDefinition(start, end)
             val tagIds = tags.map{tag -> tag.id} // 이거 맞나?
             val length = conditionRecordDao.getDefinitionList(tagIds) // 내가 짠 쿼리에 의문인데...이게 맞나? 아래 태그에 관한것도 세로축을 태그아이디를 넣고 돌리는데 데피니션인데도 태그기반으로 찾는게 맞나? 왜이렇게 했찡?
@@ -111,8 +164,11 @@ class TrackingViewModel (
         }
     }
 
-    fun conditionTrackingByTag(start: String, end: String, tags: List<ConditionTag>){ // getConditionTagList의 결과를 받음
+    fun conditionTrackingByTag(tags: List<ConditionTag>){ // getConditionTagList의 결과를 받음
         viewModelScope.launch{
+            val start = trackingUiState.startDate
+            val end = trackingUiState.endDate
+
             val tagIds = tags.map{tag -> tag.id}
             val width = conditionRecordDao.trackingByTag(tagIds, start, end)
             val length = conditionRecordDao.getTagList(tagIds)
@@ -120,8 +176,11 @@ class TrackingViewModel (
         }
     }
 
-    fun conditionMonthlyByTag(tagId: Long, start: String, end: String){
+    fun conditionMonthlyByTag(tagId: Long){
         viewModelScope.launch{
+            val start = trackingUiState.startDate
+            val end = trackingUiState.endDate
+
             val trackingData = conditionRecordDao.getMonthlyByTag(tagId, start, end)
             trackingUiState = trackingUiState.copy(conditionMonthlyByTag = trackingData)
         }
